e831d9cf5fdc2d21fb2f06a09ca566d2e758cb48
abhirajkulaar <abhirajkulaar@gmail.com>
2022-04-09 14:28:45 +0530

    Created Sample Landing Screen

---
+++ src/Routes/landing/productsList/index.js
@@ -0,0 +1,9 @@
+
+import ProductCard from "../../../Components/productTile"
+function ProductList()
+{
+    return <ProductCard price = {200} image={"https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8c2hvZXMlMjBuaWtlfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"} title="Shoes" description={"New in stock shoes"}></ProductCard>
+}
+
+
+export default ProductList
